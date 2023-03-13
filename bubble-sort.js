f9d0cf1117487ce2062965e223698122503360cb
Nick <nick.buscemi13@gmail.com>
2023-03-13 19:59:03 -0400

    bubbleSort

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,18 +1,18 @@
 
 function bubbleSort(arr) {
-
-    // Iterate through the array
-
-      // If the current value is greater than its neighbor to the right
-        // Swap those values
-
-        // Do not move this console.log
-        console.log(arr.join(","));
-
-    // If you get to the end of the array and no swaps have occurred, return
-
-    // Otherwise, repeat from the beginning
-
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+    console.log(arr.join(','));
+  }
+ 
 }
+
+bubbleSort([2,4,6,8,1,3,5,7,9])
 
 module.exports = bubbleSort;
